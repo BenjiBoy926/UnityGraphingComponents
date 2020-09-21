c9e9c6539e3250ceecc0483f73845db72f8eaea7
Codey Huntting <creativecodey@gmail.com>
2020-09-20 17:35:54 -0700

    Added an action that plays an audio clip

---
+++ Actions/PlayAudioClip.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayAudioClip : MonoBehaviour
+{
+    public Input<AudioSource> source;
+    public Input<AudioClip> clip;
+
+    public UnityEvent soundStart;
+    public UnityEvent soundEnd;
+
+    public void Invoke()
+    {
+        source.value.clip = clip.value;
+        source.value.Play();
+
+        soundStart.Invoke();
+
+        StopAllCoroutines();
+        StartCoroutine("CheckClipFinishedPlaying");
+    }
+
+    private IEnumerator CheckClipFinishedPlaying()
+    {
+        yield return new WaitUntil(() => !source.value.isPlaying);
+        soundEnd.Invoke();
+    }
+}
