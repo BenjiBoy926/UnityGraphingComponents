4bf2f7fbc37df9be0bf3f289aa25b995c37ff6e9
Codey Huntting <creativecodey@gmail.com>
2020-09-23 14:46:09 -0700

    Added the color variable

---
+++ Actions/Setters/SetColorAlpha.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SetColorAlpha : MonoBehaviour
+{
+    public Input<Color> input;
+    public Input<float> newAlpha;
+
+    public Result<Color> result;
+
+    public UnityEvent output;
+
+    public void Invoke()
+    {
+        result.value = new Color(input.value.r, input.value.g, input.value.b, newAlpha.value);
+        output.Invoke();
+    }
+}
